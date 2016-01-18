97775c5b1ee0fd11a2b256d9de94a80df1e1a900
Aviv Cohn <avivcohn123@yahoo.com>
2016-01-18 22:37:47 +0200

    Created setup.py file.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from os import path
+
+_here = path.dirname(path.abspath(__file__))
+_readme = open(path.join(_here, 'README.rst')).read()
+
+setup(
+    name='genericfuncs',
+    description='Dynamic dispatch over arbitrary predicates',
+    long_description=_readme,
+    version='1.0.0',
+    author='Aviv Cohn',
+    author_email='avivcohn123@yahoo.com',
+    license='MIT',
+    py_modules=['genericfuncs'],
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='generic functions utility programming development'
+)
