48ca38966ce6eebde138ebabdb9d90d45ca3ace8
Aviv Cohn <avivcohn123@yahoo.com>
2016-01-21 00:59:58 +0200

    Incremented to version 0.2.0.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='genericfuncs',
     description='Dynamic dispatch over arbitrary predicates',
     long_description=_readme,
-    version='0.1.1',
+    version='0.2.0',
     url='https://github.com/AvivC/genericfuncs',
     author='Aviv Cohn',
     author_email='avivcohn123@yahoo.com',
